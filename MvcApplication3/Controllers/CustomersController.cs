a16258c8213a21dc5e3234fe79c1c6fdcd9c2b8b
rabZ1 <r4bit@o2.pl>
2016-12-21 18:59:39 +0100

    wrzucenie projektu

---
+++ MvcApplication3/Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication3.Models;
+
+namespace MvcApplication3.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private northwindEntities db = new northwindEntities();
+
+        //
+        // GET: /Customers/
+
+        public ActionResult Index()
+        {
+            return View(db.Customers.ToList());
+        }
+
+    }
+}
---
+++ MvcApplication3/HtmlHelpers/CustomHtmlHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcApplication3.HtmlHelpers
+{
+    public static class CustomHtmlHelper
+    {
+        public static string MakeActive(this UrlHelper urlHelper, string controller)
+    {
+        string result = "active";
+
+        string controllerName = urlHelper.RequestContext.RouteData.Values["controller"].ToString();
+
+        if (!controllerName.Equals(controller, StringComparison.OrdinalIgnoreCase))
+        {
+            result = null;
+        }
+
+        return result;
+    }
+    }
+}
